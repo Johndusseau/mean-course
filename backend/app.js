d37570c0282c78fa9a5f3c00b854f631638c769c
john dusseau <johndusseau92@gmail.com>
2018-11-27 18:30:07 -0600

    node

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,49 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log("First middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("hello form express");
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully"
+  });
+});
+
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "nvnvofnvo",
+      title: "first server-side post",
+      content: "this is coming form the server"
+    },
+    {
+      id: "vbrwivbiupfwvu",
+      title: "second server-side post",
+      content: "this is coming form the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts feteched sucessfully",
+    posts: posts
+  });
 });
 
 module.exports = app;
